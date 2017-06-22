476a1aa58892b3c4b10b2ea228a9ef6459790da2
Mathieu-S <mathieusartorio@gmail.com>
2017-06-22 11:52:54 +0200

    Ajout fonctionnalité ajout sondage

--- src/AppBundle/Controller/SondageController.php
+++ src/AppBundle/Controller/SondageController.php
@@ -90,61 +90,111 @@ class SondageController extends Controller
      */
     public function addSondageAction(Request $request)
     {
-//        if (!$this->getUser()) {
-//            return $this->redirectToRoute("homepage");
-//        }
-
-//        if ($request->getMethod() == 'POST') {
-//
-//            $em = $this->getDoctrine()->getManager();
-//
-//            $sondage = new Sondage();
-//            $sondage->setAuteur($this->getUser());
-//            $sondage->setCreationDate(new \DateTime());
-//            $sondage->setPublier(false);
-//
-//            //Traitement image
-//            dump($_FILES);
-//            $sondage->setImage("nope");
-//
-//            //Traitement carte
-////        $sondage->setCarte(1);
-//
-//            $proposition = new Proposition();
-//            $proposition->setLabel("Test 1");
-//            $proposition->setCouleur("red");
-//            $proposition->setSondage($sondage);
-//
-//            $sondage->addProposition($proposition);
-//
-//            $em->persist($sondage);
-//            $em->flush();
-//
-//            return $this->redirectToRoute("homepage");
-//        }
-
         if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->redirectToRoute('fos_user_security_login');
         }
 
-        $sondage = new Sondage();
-        $sondage->setAuteur($this->getUser());
-        $sondage->setCreationDate(new \DateTime());
-        $sondage->setPublier(false);
+        if ($request->getMethod() == 'POST') {
+            $sondage = new Sondage();
+            $sondage->setTitre($request->request->get('titre'));
+            $sondage->setAuteur($this->getUser());
+            $sondage->setCreationDate(new \DateTime());
+            $sondage->setPublier(false);
 
-        $form = $this->get('form.factory')->create(SondageType::class, $sondage);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
             $em = $this->getDoctrine()->getManager();
-            $em->persist($data);
+            $em->persist($sondage);
+
+            if ($_POST['proposition_1']) {
+                $proposition_1 = new Proposition();
+                $proposition_1->setLabel($_POST['proposition_1']);
+                $proposition_1->setCouleur($_POST['proposition_1_color']);
+                $proposition_1->setSondage($sondage);
+                $sondage->addProposition($proposition_1);
+            }
+
+            if ($_POST['proposition_2']) {
+                $proposition_2 = new Proposition();
+                $proposition_2->setLabel($_POST['proposition_2']);
+                $proposition_2->setCouleur($_POST['proposition_2_color']);
+                $proposition_2->setSondage($sondage);
+                $sondage->addProposition($proposition_2);
+            }
+
+            if ($_POST['proposition_3']) {
+                $proposition_3 = new Proposition();
+                $proposition_3->setLabel($_POST['proposition_3']);
+                $proposition_3->setCouleur($_POST['proposition_3_color']);
+                $proposition_3->setSondage($sondage);
+                $sondage->addProposition($proposition_3);
+            }
+
+            if ($_POST['proposition_4']) {
+                $proposition_4 = new Proposition();
+                $proposition_4->setLabel($_POST['proposition_4']);
+                $proposition_4->setCouleur($_POST['proposition_4_color']);
+                $proposition_4->setSondage($sondage);
+                $sondage->addProposition($proposition_4);
+            }
+
+            if ($_POST['proposition_5']) {
+                $proposition_5 = new Proposition();
+                $proposition_5->setLabel($_POST['proposition_5']);
+                $proposition_5->setCouleur($_POST['proposition_5_color']);
+                $proposition_5->setSondage($sondage);
+                $sondage->addProposition($proposition_5);
+            }
+
+            $em->persist($sondage);
+            $em->flush();
+
+            if (!empty($_FILES['image']['name'])) {
+                $dossier = 'assets/img/uploaded/';
+                $fichier = basename($_FILES['image']['name']);
+                $taille_maxi = 1000000;
+                $taille = filesize($_FILES['image']['tmp_name']);
+                $extensions = array('.png', '.gif', '.jpg', '.jpeg');
+                $extension = strrchr($_FILES['image']['name'], '.');
+                $nameFile = "imgSondage_" . $sondage->getId() . "_" . $fichier;
+                //Début des vérifications de sécurité...
+                if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+                {
+                    $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+                }
+                if($taille>$taille_maxi)
+                {
+                    $erreur = 'Le fichier est trop gros...';
+                }
+                if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+                {
+                    //On formate le nom du fichier ici...
+                    $fichier = strtr($fichier,
+                        'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+                        'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+                    $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+                    if(move_uploaded_file($_FILES['image']['tmp_name'], $dossier . $nameFile)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+                    {
+                        echo 'Upload effectué avec succès !';
+                    }
+                    else //Sinon (la fonction renvoie FALSE).
+                    {
+                        echo 'Echec de l\'upload !';
+                    }
+                }
+
+                $sondage->setImage($nameFile);
+            }
+
+            $carte = $this->getDoctrine()->getRepository('AppBundle:Carte')->find($_POST['carte']);
+            $sondage->setCarte($carte);
+
+            $em->persist($sondage);
             $em->flush();
         }
 
+        $cartes = $this->getDoctrine()->getRepository("AppBundle:Carte")->findAll();
+
         return $this->render("sondage/new-sondage.html.twig", [
-            'form' => $form->createView()
+            'cartes' => $cartes
         ]);
     }
 
