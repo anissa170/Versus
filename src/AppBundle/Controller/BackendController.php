1d3250596c8273bd8ed85210670f4b969bc4bc05
Mathieu-S <mathieusartorio@gmail.com>
2017-06-27 15:01:56 +0200

    Regroupement des fonction dans backend manager

--- src/AppBundle/Controller/BackendController.php
+++ src/AppBundle/Controller/BackendController.php
@@ -107,7 +107,15 @@ class BackendController extends Controller
         ]);
     }
 
-
+    /**
+     * @Route("/sondage/{id}", name="propositionsFromSondageBackend")
+     */
+    public function propositionsFromSondageAction(Sondage $sondage)
+    {
+        return $this->render('backend/propositions.html.twig', [
+            'propositions' => $sondage->getPropositions()
+        ]);
+    }
 
     /**
      * @Route("/cartes/{page}", defaults={"page": "1"}, name="cartesBackend")
@@ -134,8 +142,6 @@ class BackendController extends Controller
         ]);
     }
 
-
-
     /**
      * @Route("/cartes/{id}/region", name="regionsFromCarteBackend")
      */
@@ -147,13 +153,5 @@ class BackendController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/sondage/{id}", name="propositionsFromSondageBackend")
-     */
-    public function propositionsFromSondageAction(Sondage $sondage)
-    {
-        return $this->render('backend/propositions.html.twig', [
-            'propositions' => $sondage->getPropositions()
-        ]);
-    }
+
 }
