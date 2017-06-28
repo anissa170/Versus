0970d5228c302596022e1167018212f4c42ba2c0
Bleuh <bastiendouib@gmail.com>
2017-06-28 08:51:07 +0200

    Fix merge

--- src/AppBundle/Controller/BackendController.php
+++ src/AppBundle/Controller/BackendController.php
@@ -108,16 +108,6 @@ class BackendController extends Controller
     }
 
     /**
-     * @Route("/sondage/{id}", name="propositionsFromSondageBackend")
-     */
-    public function propositionsFromSondageAction(Sondage $sondage)
-    {
-        return $this->render('backend/propositions.html.twig', [
-            'propositions' => $sondage->getPropositions()
-        ]);
-    }
-
-    /**
      * @Route("/cartes/{page}", defaults={"page": "1"}, name="cartesBackend")
      */
     public function cartesAction($page)
